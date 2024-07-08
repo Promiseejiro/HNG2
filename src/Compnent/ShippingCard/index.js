2e0ef330f7e5aa27377027f201cbee437723b9d3
Your Name <promiseejiro43@gmail.com>
2024-07-08 15:59:55 +0100

    new commit

--- src/Compnent/ShippingCard/index.js
+++ src/Compnent/ShippingCard/index.js
@@ -1,5 +1,7 @@
 import style from "./index.module.css";
 import car from "../../../src/assets/svg/car.svg";
+import card from "../../../src/assets/svg/card.svg";
+import tone from "../../../src/assets/svg/tone.svg";
 import Typography from "../Typograhy";
 const ShippingCards = ({ icon, text, header }) => {
   return (
@@ -16,16 +18,16 @@ const ShippingCards = ({ icon, text, header }) => {
         <div className={style.item2}>
           {" "}
           <div className={style.shippingCard}>
-            <img src={car} className={style.image} />
-            <h5>Free shipping</h5>
-            <p>Enjoy free delivery on all order from our company.</p>
+            <img src={card} className={style.image} />
+            <h5>Secure payment</h5>
+            <p>Benefit from secure payment option with our company.</p>
           </div>
         </div>
         <div className={style.last}>
           <div className={`${style.shippingCard} `}>
-            <img src={car} className={style.image} />
-            <h5>Free shipping</h5>
-            <p>Enjoy free delivery on all order from our company.</p>
+            <img src={tone} className={style.image} />
+            <h5>24 hour support</h5>
+            <p>we offer 24 hour customer supportto our customers</p>
           </div>
         </div>
       </div>
